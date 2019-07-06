677238f9c7ffce24e5128a21cd4e05a135903fcd
Bartek Gwizdala <bgwizdala@myaic.com>
2019-07-06 14:55:48 +0200

    Move printing csv headers to aggregators

--- aggregator.py
+++ aggregator.py
@@ -11,14 +11,25 @@ class Aggregator:
         self.response_time_sum = 0.
 
     def process(self, log):
-        if not self.regex_pattern or \
-           not self.log_attribute_name or \
-           re.match(self.regex_pattern, getattr(log, self.log_attribute_name)):
+        if self.is_default_aggregator or self.matches_log(log):
             self.request_count += 1
             self.response_time_sum += log.request_time
 
     def columns(self):
         return [self.request_count, self.average_response_time]
+
+    def headers(self):
+        default_headers = ['req_cnt', 'avg_resp_time']
+        if not self.is_default_aggregator:
+            return ['{}_{}_{}'.format(header, self.log_attribute_name, self.regex_pattern) for header in default_headers]
+        return default_headers
+    
+    def matches_log(self, log):
+        return re.match(self.regex_pattern, getattr(log, self.log_attribute_name))
+
+    @property
+    def is_default_aggregator(self):
+        return not self.regex_pattern or not self.log_attribute_name
 
     @property
     def average_response_time(self):
--- interval_stats_collector.py
+++ interval_stats_collector.py
@@ -8,35 +8,35 @@ class IntervalStatsCollector:
     def __init__(self, start_datetime):
         self.start_datetime = start_datetime
         self.logs = []
+        self.aggregators = self.create_aggregators()
 
     def append(self, log):
         self.logs.append(log)
 
     def close(self):
-        aggregators = [Aggregator()]
-
-        for regex_pattern, log_attribute_name in categories():
-            aggregators.append(Aggregator(regex_pattern, log_attribute_name))
-
         for log in self.logs:
-            for aggregator in aggregators:
+            for aggregator in self.aggregators:
                 aggregator.process(log)
 
         columns = [self.start_datetime]
 
-        for aggregator in aggregators:
+        for aggregator in self.aggregators:
             columns.extend(aggregator.columns())
         
         print(';'.join(map(str, columns)))
 
     @staticmethod
-    def print_headers():
-        columns = ['datetime',
-                   'req_cnt',
-                   'avg_resp_time']
-
+    def create_aggregators():
+        aggregators = [Aggregator()]
         for regex_pattern, log_attribute_name in categories():
-            columns.append('req_cnt_{}_{}'.format(log_attribute_name, regex_pattern))
-            columns.append('avg_resp_time_{}_{}'.format(log_attribute_name, regex_pattern))
+            aggregators.append(Aggregator(regex_pattern, log_attribute_name))
+        return aggregators
 
+    @staticmethod
+    def print_headers():
+        columns = ['datetime']
+
+        for aggregator in IntervalStatsCollector.create_aggregators():
+            columns.extend(aggregator.headers())
+        
         print(';'.join(columns))
