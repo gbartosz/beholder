7ce4888ecbef8c9d9f2fa3687f47cdafb5940bd0
Bartek Gwizdala <bgwizdala@myaic.com>
2019-07-06 11:32:39 +0200

    Update log.py

--- log.py
+++ log.py
@@ -20,12 +20,16 @@ class Log:
         self.upstream_address = matches[6]
         self.upstream_response_time = float(matches[7]) if matches[7] != '-' else 0.
 
+    #[02/Jul/2019:16:49:35 +0200] 83.210.40.132 (#1) "POST /mainapi/ HTTP/1.1" request_time: 1.396 status: 201 bytes: 959 "POST /mainapi/ID00611623 HTTP/1.1" to: 83.210.0.85:8000 upstream_response_time: 1.396'
     def __repr__(self):
-        return (8*"{}\t").format(self.datetime,
-                                 self.client_address,
-                                 self.method, 
-                                 self.request_time, 
-                                 self.status_code, 
-                                 self.upstream_address, 
-                                 self.upstream_response_time, 
-                                 self.endpoint)
+        return """[{} +0000] {} (#1) "{} {} HTTP/1.1" request_time: {:.3f} status: {} bytes: 112 "{} {} HTTP/1.1" to: {} upstream_response_time: {:.3f}"""\
+                .format(self.datetime.strftime('%d/%b/%Y:%H:%M:%S'),
+                        self.client_address,
+                        self.method,
+                        self.endpoint,
+                        self.request_time,
+                        self.status_code,
+                        self.method,
+                        self.endpoint,
+                        self.upstream_address,
+                        self.upstream_response_time)
